f96407a5635d66ec9dee52ae42b3f33cdb0a80cc
Mina <mina.dodunekova@gmail.com>
2014-07-04 22:13:44 +0300

    Deleted one unnecessary comment
    No worries if you have uploaded the old version of the file in the SoftUni form.

--- site/perform_check.php
+++ site/perform_check.php
@@ -4,7 +4,7 @@ session_start();
 $userval = strtolower(trim($_POST['captchavalue']));
 $thecapt = strtolower($_SESSION['captcha']);
 
-if( $userval == $thecapt ) { //&& count($error) == 0 
+if( $userval == $thecapt ) {
     echo "true"; 
 } else { 
     echo "false";
